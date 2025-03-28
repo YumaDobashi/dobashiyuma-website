2bd4d8034d33b87531af4630559d20b43779b7fc
YumaDobashi <roku.records.japan@gmail.com>
2025-03-28 13:39:08 +0900

    タイトルフェード案

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+      const official = document.getElementById("official-text");
+      if (official) {
+        official.classList.add("visible");
+      }
+    }, 2000); // 2秒後にフェードイン（調整可）
+  });
+  
