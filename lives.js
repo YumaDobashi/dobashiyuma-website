386b4c0163abd6ba36b7aa6703222354a05da46b
YumaDobashi <roku.records.japan@gmail.com>
2025-07-27 22:49:00 +0900

    Update site

--- lives.js
+++ lives.js
@@ -1,38 +1,13 @@
-  const lives = [
-    {
-      date: "2025年4月6日（日）",
-      dateRaw: "2025-04-06", // ← 判別用
-      venue: "焼き鳥元ちゃん（富士宮）",
-      title: "「BROS FEST.への導き」",
-      acts: "土橋悠宇 / dsk.og",
-      ticket: "¥2,000（+1D）",
-      time: "16:00 開場 / 17:00 開演",
-      note: "",
-      flyer: "images/flyer-sample1.jpg"
-    },
-
-    {
-        date: "2025年4月19日（土）",
-        dateRaw: "2025-04-19",
-        venue: "RINCOLO（御殿場）",
-        title: "「単独公演2025春」",
-        acts: "土橋悠宇",
-        ticket: "¥2,000（+1D）",
-        time: "18:00 開場 / 19:00 開演",
-        note: "",
-        flyer: "images/flyer-sample2.jpg"
-      },
- 
-
-    {
-      date: "2025年3月8日（土）", // ← 仮の終了ライブ
-      dateRaw: "2025-03-08",
-      venue: "架空ホール",
-      title: "「幻の公演 - 春のまぼろし」",
-      acts: "土橋悠宇",
-      ticket: "¥0",
-      time: "00:00",
-      note: "（このライブは終了しました）",
-      flyer: "images/flyer-sample2.jpg"
-      },
+const lives = [
+  {
+    date: "2025年8月2日（土）",
+    dateRaw: "2025-08-02",
+    venue: "surfers（逗子市新宿5-822-2）",
+    title: "BRISA MARINA 〜日焼け注意報〜",
+    acts: "BOBOBO LTD / NAOYUKI with Shuya & Yuki / RyuTheDaddy acoustic set / 土橋悠宇",
+    ticket: "前売 ¥3,500 / 当日 ¥4,000",
+    time: "16:30 開場 / 17:00 開演",
+    note: "¡Cantamos, bailamos y brindamos!",
+    flyer: "images/flyer-2025-08-02.jpg"
+  },
 ];
